a35c76ff671138657968100351daf07257627208
Rushabh1134 <rushabhpatil557@gmail.com>
2024-10-07 00:39:24 +0530

    Add @vercel/analytics dependency and integrate analytics component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
